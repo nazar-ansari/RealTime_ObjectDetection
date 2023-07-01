b32578d8cc094cbf6a10cd9628141c8c4c956e17
Nazar Ansari <nazaransari06@gmail.com>
2023-07-01 21:34:22 +0530

    Added Required Libraries for Project

---
+++ src/Object_Detection.py
@@ -0,0 +1,7 @@
+# Importing Required Module for Project
+
+import numpy as np
+from ultralytics import YOLO
+import cv2 as vision
+import cvzone
+from sort import * // https: // github.com/abewley/sort
