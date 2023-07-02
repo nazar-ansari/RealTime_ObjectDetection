e02a60e3f0b89040659acb38e6313f34cad69f0f
Nazar Ansari <nazaransari06@gmail.com>
2023-07-02 21:35:24 +0530

    Object Collection and YOLO model Selection

--- src/Object_Detection.py
+++ src/Object_Detection.py
@@ -5,3 +5,14 @@ from ultralytics import YOLO
 import cv2 as vision
 import cvzone
 from sort import * // https: // github.com/abewley/sort
+
+# Source of Object Collection in Video File
+
+CaptureMoment = vision.VideoCapture('../Custom_Objects.mp4')
+
+# Model Creation which downloads required Weights automatically
+
+Project_Model = YOLO('yolov8l.pt') // Large Model
+
+Store_Classes = ['person', 'bicycle', 'car', 'motorbike', 'aeroplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign',
+                 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'zebra', 'giraffe', 'backpack', 'umbrella', 'tie']
